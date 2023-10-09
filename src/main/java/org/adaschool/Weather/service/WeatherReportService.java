e02b5c224e106e44f4651e1f96ba0db082ed3a88
Santiago Cardenas <cardenas.santiago@hotmail.com>
2023-10-09 14:14:20 -0500

    weather-test | Terminado

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "99bf977001a12e4563fbdd5b1052862a";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public WeatherReport getWeatherReport(double latitude, double longitude) {
--- src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
+++ src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
@@ -1,13 +1,46 @@
 package org.adaschool.Weather;
 
+import org.adaschool.Weather.controller.WeatherReportController;
+import org.adaschool.Weather.data.WeatherApiResponse;
+import org.adaschool.Weather.data.WeatherReport;
+import org.adaschool.Weather.service.WeatherReportService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class WeatherApplicationTests {
+	@Mock
+	private WeatherReportController weatherReportController;
+
+	@InjectMocks
+	private WeatherReportService weatherReportService;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testGetWeatherReport() {
+		// Crear un objeto WeatherReport simulado
+		WeatherReport weatherReport = new WeatherReport();
+		WeatherApiResponse.Main main = new WeatherApiResponse.Main();
+		main.setHumidity(53.0);
+		main.setTemperature(0.0);
+		weatherReport.setHumidity(main.getHumidity());
+		weatherReport.setTemperature(main.getTemperature());
 
+		// Configurar el comportamiento simulado del controlador
+		Mockito.when(weatherReportController.getWeatherReport(37.8267, -122.4233))
+				.thenReturn(weatherReport);
+
+		// Llamar al método que se está probando
+		WeatherReport weatherReportRes = weatherReportController.getWeatherReport(37.8267, -122.4233);
+
+		// Comprobar que los valores devueltos coinciden con los esperados
+		Assertions.assertEquals(weatherReport.getHumidity(), weatherReportRes.getHumidity());
+		Assertions.assertEquals(weatherReport.getTemperature(), weatherReportRes.getTemperature());
+	}
 }
